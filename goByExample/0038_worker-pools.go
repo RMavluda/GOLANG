df5e6134bd4b92368092a3e5469bffd7dcac7fc1
Mavluda <rahimqulimavluda@gmail.com>
2024-01-27 16:43:09 +0500

    worker pools

--- goByExample/0038_worker-pools.go
+++ goByExample/0038_worker-pools.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
+func workers(id int, jobs <-chan int, results chan<- int) {
 	for j := range jobs {
 		fmt.Println("worker", id, "started job", j)
 		time.Sleep(time.Second)
@@ -21,7 +21,7 @@ func main() {
 	results := make(chan int, numJobs)
 
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+		go workers(w, jobs, results)
 	}
 
 	for j := 1; j <= numJobs; j++ {
