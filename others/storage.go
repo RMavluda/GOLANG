a5dcba17e4566b8f5107c25a4ebd67b197444157
Mavluda <rahimqulimavluda@gmail.com>
2024-02-06 16:28:09 +0500

    rest-api, json, go.mod, gin-gonic

--- others/storage.go
+++ others/storage.go
@@ -1,42 +1,41 @@
 package main
 
-//import 	"sync"
-//
-//type Employee struct {
-//	ID 		int		'json:"id"'
-//	Name 	string 	'json:"name"'
-//	Sex		string	'json:"sex"'
-//	Age		int		'json:"age"'
-//	Salary	int		'json:"salary"'
-//}
-//
-//type Storage interface{
-//	Insert(e *Employee)
-//	Get(id int) (Employee, error)
-//	Update(id int, e Employee)
-//	Delete(id int)
-//}
-//
-//type MemoryStorage struct{
-//	counter int
-//	data map[int]Employee
-//	sync.Mutex
-//}
-//
-//func NewMemoryStorage() *MemoryStorage {
-//	return &MemoryStorage{
-//		data: make(map[int]Employee),
-//		counter: 1,
-//	}
-//}
-//
-//func (s *MemoryStorage) Insert(e *Employee) {
-//	s.Lock()
-//
-//	e.ID = s.counter
-//	s.data[e.ID] = *e
-//	s.counter++
-//
-//	s.Unlock()
-//}
-//
+import "sync"
+
+type Employee struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Sex    string `json:"sex"`
+	Age    int    `json:"age"`
+	Salary int    `json:"salary"`
+}
+
+type Storage interface {
+	Insert(e *Employee)
+	Get(id int) (Employee, error)
+	Update(id int, e Employee)
+	Delete(id int)
+}
+
+type MemoryStorage struct {
+	counter int
+	data    map[int]Employee
+	sync.Mutex
+}
+
+func NewMemoryStorage() *MemoryStorage {
+	return &MemoryStorage{
+		data:    make(map[int]Employee),
+		counter: 1,
+	}
+}
+
+func (s *MemoryStorage) Insert(e *Employee) {
+	s.Lock()
+
+	e.ID = s.counter
+	s.data[e.ID] = *e
+	s.counter++
+
+	s.Unlock()
+}
